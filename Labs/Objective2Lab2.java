f53bbbb73d038e1cd2dcba53a7ab98f72137bc20
Miles Porter <portermiles91@gmail.com>
2025-02-27 21:59:48 -0700

    completed Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Miles Porter";
+		int age = 33;
+		String state = "Texas";
+
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(state);
+
+	}
+}
