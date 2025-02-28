fa1c29a982f4ea01dc55bddd450abe01592c2c07
Miles Porter <portermiles91@gmail.com>
2025-02-28 09:39:07 -0700

    completed Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1, side2, hypotenuse;
+
+		side1 = 12;
+		side2 = 15;
+		hypotenuse = Math.sqrt((side1 * side1) + (side2 * side2));
+		System.out.println("The hypotenuse of a triangle with sides 12.0 and 15.0 is " + hypotenuse);
+
+	}
+}
