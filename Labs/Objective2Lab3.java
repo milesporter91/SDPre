511a3c85ff9401163a6ed292000e343e88726c02
Miles Porter <portermiles91@gmail.com>
2025-02-27 22:16:33 -0700

    completed Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		
+		int year = 1991;
+		String month = "October";
+		boolean weekend = false;
+
+		System.out.println(year);
+		System.out.println(month);
+		System.out.println(weekend);
+
+	}
+}
