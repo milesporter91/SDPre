916ca91955a0306f5499cde513f8e141cf5919d5
Miles Porter <portermiles91@gmail.com>
2025-03-01 10:36:44 -0700

    completed Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int sum, count;
+		sum = 0;
+		count = 1;
+
+		while(count <=20) {
+			sum = count + sum;
+			count = count + 1;
+
+		}
+			System.out.println(sum);
+
+	}
+}
