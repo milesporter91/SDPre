f7f31533b374dfffbe3616018ba2c568a654607b
Miles Porter <portermiles91@gmail.com>
2025-02-28 20:27:48 -0700

    completed Objective5Lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Objective5Lab1 {
+
+	public static void main(String[] args) {
+		int num1 = 12;
+		int num2 = 8;
+
+		if(num1 > num2) {
+			System.out.println(num1 + " is greater than " + num2);
+		}
+		else if(num2 > num1) {
+			System.out.println(num2 + " is greater than " + num2);
+		}
+
+	}
+}
